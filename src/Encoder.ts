e22857366b74712d32fe76a68c32d5742bd662a5
Dmitriy_Narzyaev <nozaki@yandex.ru>
2021-07-24 16:26:55 +0300

    refactoring encoder

--- src/Encoder.ts
+++ src/Encoder.ts
@@ -1,16 +1,15 @@
 import Container = PIXI.Container;
 
 export default class Encoder extends Container {
-private targetSymbols:string[] = [
+	private _targetSymbols:string[] = [
 		"а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н",
 		"о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь",
 		"э", "ю", "я", "(", ")", "-", "—"
 	];
-	private codeSymbols:string[] = [
-		"1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "a", "b", "d", "e",
+	private _codeSymbols:string[] = [
+		"1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/", "a", "b", "d", "e",
 		"f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
 		"u", "v", "w", "x", "y", "z", "%",
-		//"Ⴧ", "Ⴥ",
 	];
 
 	constructor() {
@@ -18,47 +17,32 @@ private targetSymbols:string[] = [
 	}
 
 	public encodeText(text:string):string {
-		let encodedText:string = "";
-		for (let iterator:number = 0; iterator < text.length; iterator ++) {
-			let targetLetter:string = text[iterator];
-			encodedText += this.encodeLetter(targetLetter);
-		}
-        return encodedText;
+		return this.convertText(text, this._targetSymbols, this._codeSymbols);
 	}
 
 	public decodeText(text:string):string {
-		let decodedText:string = "";
+		return this.convertText(text, this._codeSymbols, this._targetSymbols);
+	}
+
+	private convertText(text:string, arrayFrom:string[], arrayTo:string[]):string {
+		let convertedText:string = "";
 		for (let iterator:number = 0; iterator < text.length; iterator ++) {
 			let targetLetter:string = text[iterator];
-			decodedText += this.decodeLetter(targetLetter);
+			convertedText += this.convertLetter(targetLetter, arrayFrom, arrayTo);
 		}
-        return decodedText;
+        return convertedText;
 	}
 
-	private encodeLetter(letter:string):string {
-		let codeLetter:string;
+	private convertLetter(letter:string, arrayFrom:string[], arrayTo:string[]):string {
+		let convertedLetter:string;
 		letter = letter.toLowerCase();
-		let charIndex:number = this.targetSymbols.indexOf(letter);
+		let charIndex:number = arrayFrom.indexOf(letter);
 
 		if (charIndex != -1) {
-			codeLetter = this.codeSymbols[charIndex];
+			convertedLetter = arrayTo[charIndex];
 		} else {
-			codeLetter = letter;
+			convertedLetter = letter;
 		}
-		return codeLetter;
-	}
-
-	private decodeLetter(letter:string):string {
-		let codeLetter:string;
-		letter = letter.toLowerCase();
-		let charIndex:number = this.codeSymbols.indexOf(letter);
-		console.log(letter);
-
-		if (charIndex != -1) {
-			codeLetter = this.targetSymbols[charIndex];
-		} else {
-			codeLetter = letter;
-		}
-		return codeLetter;
+		return convertedLetter;
 	}
 }
